314b4ff223a17bcbb20fbe102b20095b2e901256
Khoi Nguyen <ndkhoi.dev@gmail.com>
2024-03-19 05:37:52 +0700

    implement get current signed in user

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['eaiofwdj'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove additional property from request that not exist in dto
